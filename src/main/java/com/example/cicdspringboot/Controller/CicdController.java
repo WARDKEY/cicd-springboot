a0d9cb09d859fad218c3f6efd68ac9e9765e6a83
WARDKEY <tjrmsduq@naver.com>
2024-06-12 22:34:58 +0900

    cicd test

---
+++ src/main/java/com/example/cicdspringboot/Controller/CicdController.java
@@ -0,0 +1,15 @@
+package com.example.cicdspringboot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class CicdController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "Hello, CICD!";
+	}
+}
